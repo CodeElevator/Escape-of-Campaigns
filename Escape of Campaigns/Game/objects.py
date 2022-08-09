221b1efaa6a9438b95e67d9d2345ee7d36bb325f
CodeElevator <codeelvator131@gmail.com>
2022-08-09 06:59:38 -0400

    Added enemies and db integrations

--- Escape of Campaigns/Game/objects.py
+++ Escape of Campaigns/Game/objects.py
@@ -1,3 +1,4 @@
+import sqlite3
 # Initialize items
 class ItemClass():
     def __init__(self, name: str, description: str):
@@ -6,6 +7,13 @@ class ItemClass():
         if self.name and self.description is None:
             self.name = 'Item'
             self.description = 'This is an item that has no name and no description..strange...'
+
+# Initialize entities
+class Entity(ItemClass):
+    def __init__(self, name: str, description: str, hp: int = 100, damage: int = 30):
+        super().__init__(name, description)
+        self.hp = hp
+        self.damage = damage
 
 # Initialize potions
 class Potion(ItemClass):
@@ -22,12 +30,16 @@ class Weapon(ItemClass):
 # Initialize the objects
 class Sword(Weapon):
     def __init__(self):
-        super().__init__("Sword", 40, "This is a sword, it gives many damages to easily defeat enemies.")
+        super().__init__("Sword", 40, "This is a widely used sword used by elite soldiers in the kingdom.")
 
 class HealthPotion(Potion):
     def __init__(self):
-        super().__init__(name='Health Potion', description='This potion gives you 50 HP, so half of your health.', effect='health')
+        super().__init__(name='Health Potion', description='This potion gives you 50 HP.', effect='health')
 
 class DamagePotion(Potion):
     def __init__(self):
         super().__init__(name='Damage Potion', description='This potion takes 30 HP from you.', effect='damage')
+
+enemy = Entity('First enemy', 'This is your first enemy to fight in the tutorial')
+
+# TODO: Setup the database for items and enemies
